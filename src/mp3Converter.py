139d5fbb7490fc0672f37174ed5a1b7fac81d2f1
Zepei Zhao <84900702+Zepeiz@users.noreply.github.com>
2023-06-13 22:13:23 +0200

    Fixed crash due to invalid characters in video titleand/or channel name such as / : ! < > \ % ? * .

--- src/mp3Converter.py
+++ src/mp3Converter.py
@@ -1,10 +1,13 @@
 # importing packages
 import math
+import re
 from pytube import YouTube
 import os
 from pydub import AudioSegment
 
 class mp3Converter:
+    # Define the pattern to match the invalid symbols for filenames
+    invalid_symbols = r'[\\/:<>"|?*]'
 
 
     def download(self, yt):
@@ -16,7 +19,12 @@ class mp3Converter:
         destination = self.destinationPicker()
 
         # download the file
+
         out_file = file.download(output_path=destination)
+
+        #Check for invalid characters and remove such characters
+        if self.contains_invalid_symbols(yt.title):
+            out_file = self.rename(out_file)
 
         # rename the file
         self.extractMP3(yt, out_file)
@@ -75,12 +83,18 @@ class mp3Converter:
     def extractMP3(self, yt, out_file):
         file_path = os.path.abspath(out_file)
         audio = AudioSegment.from_file(file_path, format='mp4')
+        author = yt.author
 
         #Changing the file name
         directory_path = os.path.dirname(out_file)
         file_name = os.path.basename(out_file)
         name, extension = os.path.splitext(file_name)
-        new_file_name = yt.author + ' - ' + name + '.mp3'
+
+        #Check for invalid characters and remove such characters
+        if self.contains_invalid_symbols(author):
+            author = self.filter_file_name(author)
+
+        new_file_name = author + ' - ' + name + '.mp3'
         new_file = os.path.join(directory_path, new_file_name)
 
         #Exporting the mp3 into the new file path
@@ -89,9 +103,36 @@ class mp3Converter:
         #Deletes the now useless mp4 source file
         os.remove(file_path)
 
-    def defaultFileName(self, out_file):
-        name, extension = os.path.splitext(out_file)
-        new_file = name + '.mp3'
+    def rename(self, out_file):
+        #First separate directory path with file name
+        #Then separate file name with its extension
+        #Filter the file name, then join back the extension
+        #Then join back the new file name with the same directory as before
+        #Then replace the old file with the new file
+
+        directory_path = os.path.dirname(out_file)
+        base_name = os.path.basename(out_file)
+
+        name, extension = os.path.splitext(base_name)
+        new_name = self.filter_file_name(name)
+        new_basename = new_name + extension
+        new_file = os.path.join(directory_path, new_basename)
         os.rename(out_file, new_file)
+        return new_file
+
+    def filter_file_name(self, file_name):
+
+        # Remove the invalid symbols from the file name
+        filtered_name = re.sub(self.invalid_symbols, '', file_name)
+
+        return filtered_name
+
+    def contains_invalid_symbols(self, file_name):
+
+        # Check if the file name contains any invalid symbols
+        if re.search(self.invalid_symbols, file_name):
+            return True
+        else:
+            return False
 
 
